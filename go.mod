5bc098f8d6bd05b8a8814c8d3a71fca4d88a6670
Bao Vo <thebao0964@gmail.com>
2022-06-09 14:22:07 +0700

    fdafda

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Bao323-bit/bookstore_users-api
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/Bao323-bit/bookstore_users-api/app"
+)
+
+func main() {
+	app.StartApplication()
+}
